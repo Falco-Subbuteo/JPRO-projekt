2cb7f7eabc0d6a15da5c2b95065f14db21b04523
Bartłomiej Sokołowski <bartlomiej_sokolowski@poczta.pl>
2019-05-23 13:06:51 +0200

    Zaimplementowano funkcję rysującą planszę do gry.

--- Projekt.c
+++ Projekt.c
@@ -1,8 +1,43 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void board(int a, int b, int tab[15][15]){ //funkcja rysuj¹ca planszê (wraz z ¿ywymi komórkami).
+	system("cls");
+	int i, j;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			if (tab[i][j] == 0){
+				printf(" ");
+			} else {
+				printf("O");
+			}
+		}
+		printf("\n");
+	}
+}
+	
+	
 
 int main(){
+	//zmienne, które bêd¹ potem wczytywane z pliku
+	int a = 15, b = 15;
 	
-	printf("Projekt.\n");
+	
+	
+	int place[15][15]={0}; //plansza do gry
+	place[9][13] = 1;
+	place[2][5] = 1;
+	
+	board(a, b, place);
+	
+	
+	
+	
+	
+//	printf("Projekt.\n");
+	
+	
 	
 	return 0;
 }
