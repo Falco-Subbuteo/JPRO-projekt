19964e4a8c838d9db2ed3d479db0ed41f8d92c21
Bartłomiej Sokołowski <bartlomiej_sokolowski@poczta.pl>
2019-05-24 13:50:56 +0200

    Z zajęć. Nie udało mi się jeszcze dobrze zalokować tych tablic o rozmiarach definiowanych przez zmienne.

--- Projekt.c
+++ Projekt.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 
-void board(int a, int b, int tab[15][15]){ //funkcja rysuj¹ca planszê (wraz z ¿ywymi komórkami).
+void board(int a, int b, int **tab){ //funkcja rysuj¹ca planszê (wraz z ¿ywymi komórkami).
 	system("cls");
 	int i, j;
 	for (i=0; i<a; i++){
@@ -22,18 +22,23 @@ void board(int a, int b, int tab[15][15]){ //funkcja rysuj¹ca planszê (wraz z ¿y
 int main(){
 	//zmienne, które bêd¹ potem wczytywane z pliku
 	int a = 15, b = 15;
+	int c, i, j;
+	int **place;
 	
+	place = calloc(a, sizeof(int));
+	for (c=0; c<a; c++){
+		place[c] = calloc(b, sizeof(int));
+	}
 	
-	
-	int place[15][15]={0}; //plansza do gry
+	for (i=0; i<a; i++){
+		for(j=0; j<b; j++){ 
+			place[i][j] = 0;
+		}
+	}
 	place[9][13] = 1;
 	place[2][5] = 1;
 	
 	board(a, b, place);
-	
-	
-	
-	
 	
 //	printf("Projekt.\n");
 	
