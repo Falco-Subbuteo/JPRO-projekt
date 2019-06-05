ee9ede82080bf5ae45ed5022948ed831e6c0b1fc
Bartłomiej Sokołowski <bartlomiej_sokolowski@poczta.pl>
2019-06-05 21:20:50 +0200

    Dodano zapis wyników do pliku. Obecny problem z właścią zmianą stanów komórek.

--- Projekt.c
+++ Projekt.c
@@ -19,13 +19,15 @@ void board(int a, int b, int **tab){ //funkcja rysuj¹ca planszê (wraz z ¿ywymi k
 	}
 //}
 
+
+
 int state(int **tab, int a, int b){
 	int c, i, j, x, y, count=0, res, stop=0, diff1=0, diff2=0;
 	int **prev;
 	
 	prev = calloc(a+2, sizeof(int*));
 	for (c=0; c<a+2; c++){
-		prev[c] = calloc(b+2, sizeof(int));
+		prev[c] = calloc(b+2, sizeof(int*));
 	}
 	/*Przepisanie aktualnego stanu planszy, do drugiej tablicy tablic,
 	ktora bedzie oznaczac stan poprzedniego kroku.*/
@@ -38,37 +40,26 @@ int state(int **tab, int a, int b){
 	for (i=1; i<=a; i++){
 		for (j=1; j<=b; j++){
 			count = 0;
-			if (tab[i][j]==1){
-				count--;
-				for (y=i-1; y<=i+1; y++){
-					for (x=j-1; x<=j+1; x++){
-					//	printf("%i %i", i, j);
-						if (tab[y][x]==1){
-							count++;
-						}
+			for (y=i-1; y<=i+1; y++){
+				for (x=j-1; x<=j+1; x++){
+					if (tab[y][x]==1){
+						count++;
 					}
-				}
-				if (count==2 || count == 3){
-					res = 1;
-				} else {
-					res = 0;
-				}
-			count = 0;
-			} else if (tab[i][j]==0){
-				for (y=i-1; y<=i+1; y++){
-					for (x=j-1; x<=j+1; x++){
-						if (tab[y][x]==1){
-							count++;
-						}
-					}
-				}
-				if (count == 3){
-					res = 1;
-				} else {
-					res = 0;
 				}
 			}
-			tab[i][j] = res;
+			if (tab[i][j] == 1) {
+				if (count == 1 || count == 2){
+					tab[i][j] = 1;
+				} else {
+					tab[i][j] = 0;
+				}
+			} else if (tab[i][j] == 0) {
+				if (count == 3) {
+					tab[i][j] = 1;
+				} else {
+					tab[i][j] = 0;
+				}
+			}
 		}
 	}
 	/*Porownanie kroku poprzedniego z krokiem nastepnym,
@@ -83,10 +74,14 @@ int state(int **tab, int a, int b){
 	//Jesli diff1 i diff2 sa rowne 0, oznacza to, ze krok poprzedni i krok nastepny sa takie same.
 	if (diff1 == diff2){
 		stop = 1;
+	} else {
+		stop = 0;
 	}
 	
 	return stop;
 }
+
+
 
 			
 int random(int m){
@@ -102,12 +97,12 @@ int random(int m){
 
 int main(){
 	//zmienne, których czêœæ ma byæ potem wczytywana z pliku
-	int a = 20, b = 20, hm = 32, x, y, c, i, j, count=0, halt=0;
+	int a = 20, b = 20, hm = 100, x, y, c, i, j, count=0, halt=0, alive=0, dead=0;
 	int **place;
 	
 	place = calloc(a+2, sizeof(int*));
 	for (c=0; c<a+2; c++){
-		place[c] = calloc(b+2, sizeof(int));
+		place[c] = calloc(b+2, sizeof(int*));
 	}
 	
 	for (i=0; i<a+2; i++){
@@ -125,16 +120,53 @@ int main(){
 		}
 	}	
 	
+	
+	//FILE *data = NULL;
+	FILE *output = NULL;
+	//data = fopen("config.dat", "r");
+	output = fopen("data.dat", "w+");
+	
+	//inna metoda
+	if(/*data == NULL || */output == NULL) {
+	perror("File opening error!\n"); //funkcja wywalajaca komentarz o bledzie
+	return 1; //wyjscie z programu cokolwiek innego w return niz zero, zeby bylo wiadomo w ktorym momencie wyszlo z programu
+	}
+	
 //	board(a, b, place);
+//	sleep(1);
+//	for (i=1; i<=a; i++){
+//		for(j=1; j<=b; j++){
+//			if (place[i][j] == 1){
+//				alive++;
+//			} else {
+//				dead++;
+//			}
+//		}
+//	}
+//	fprintf(output, "%i %i %i\n", count, dead, alive);
 
 	
 	do{
-		board(a, b, place);
-		halt = state(place, a, b);
+	//	board(a, b, place);
+		dead = 0;
+		alive = 0;		
+		for (i=1; i<=a; i++){
+			for(j=1; j<=b; j++){
+				if (place[i][j] == 1){
+					alive++;
+				} else {
+					dead++;
+				}
+			}
+		}
 	//	printf("%i", count);
+		fprintf(output, "%i %i %i\n", count, dead, alive);
+		halt = state(place, a, b);
+		board(a, b, place);
 		count++;
-		sleep(1);
-	//	system("pause");
+		
+	//	sleep(1);
+		system("pause");
 	} while (halt != 1);
 	
 	printf("Game ended in %i steps.\n", count);	
--- README.txt
+++ README.txt
@@ -1,1 +1,37 @@
-Projekt JPRO.
+---------------- G A M E   O F   L I F E ----------------
+
+
+This is a readme file for programming languages project.
+
+Repository contains source code (written in C) for Conway's Game of Life.
+
+1. What is "Game of Life"?
+
+"The Game of Life, also known simply as Life, is a cellular automaton devised by the British mathematician John Horton Conway in 1970.
+The game is a zero-player game, meaning that its evolution is determined by its initial state, requiring no further input.
+One interacts with the Game of Life by creating an initial configuration and observing how it evolves, or, for advanced players, by creating patterns with particular properties (via Wikipedia).
+
+	Rules:
+1. Any living cell with 2 or 3 living neighbours stays alive (ah, ah, ah, ah...) in next step.
+2. Living cells with less than 2 or more than 3 living neighbours dies in next step.
+3. Any Dead cell with exactly 3 living neighbours becomes a living cell.
+
+Game takes place on two-dimensional place, consisting of dead or alive cells.
+
+2. Program.
+The game's place consist of two-dimensional array, which size is determined by user, via config.dat //TODO
+Initial number of living cells is also determined by user via config.dat. //TODO
+Positions of that cells on game's array is determined by pseudo-random numbers generated within program.
+After every second, the program puts actual state of array to standard output. Also in every state
+number of current step, number of dead and living cells is written to the "output.dat" file.
+The game ends when state within becomes balanced (there are no changes in placement of cells),
+or by user's demand. //TODO 
+
+3. Author.
+Bartłomiej Sokołowski
+bartlomiej_sokolowski@poczta.pl
+https://github.com/Falco-Subbuteo
++48 693 380 593
+
+
+
