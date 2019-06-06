8fc4065c34e79860228a4352d2025bcde37cae04
Bartłomiej Sokołowski <bartlomiej_sokolowski@poczta.pl>
2019-06-06 22:03:16 +0200

    Odśmiecono kod

--- Projekt.c
+++ Projekt.c
@@ -3,6 +3,8 @@
 #include <time.h>
 #include <unistd.h>
 
+	//WERSJA ALFA
+
 void board(int a, int b, int **tab){ //funkcja rysuj¹ca planszê (wraz z ¿ywymi komórkami).
 	system("cls");
 	int i, j;
@@ -16,12 +18,11 @@ void board(int a, int b, int **tab){ //funkcja rysuj¹ca planszê (wraz z ¿ywymi k
 		}
 		printf("\n");
 	}
-	printf("Press \"e\" key, to stop.\n");
 }
 
 
 
-int state(int **tab, int a, int b){
+int state(int **tab, int a, int b){ //Funkcja odpowiadaj¹ca za ewolucjê ca³ego uk³adu.
 	int c, i, j, x, y, count=0, stop=0, diff1=0, diff2=0;
 	int **prev, **res;
 	
@@ -49,14 +50,11 @@ int state(int **tab, int a, int b){
 			count = 0;
 			for (y=i-1; y<=i+1; y++){
 				for (x=j-1; x<=j+1; x++){
-	//				printf("%i ", tab[y][x]);
 					if (tab[y][x]==1){
 						count++;
 					}
 				}
-	//			printf("\n");
 			}
-//			printf("%i -----------------\n", count); //dot¹d jest ok
 			if (count == 3 && tab[i][j] == 1){
 				res[i][j] = 1;
 			} else if (count == 4 && tab[i][j] == 1){
@@ -66,7 +64,6 @@ int state(int **tab, int a, int b){
 			} else {
 				res[i][j] = 0;
 			}
-			//////////////////////////
 		}
 	}
 	
@@ -75,21 +72,6 @@ int state(int **tab, int a, int b){
 			tab[i][j] = res[i][j];
 		}
 	}
-	/*
-	for (i=1; i<=a; i++){
-		for (j=1; j<=b; j++){
-			printf("%i ", res[i][j]);
-		}
-		printf("\n");
-	}
-//	printf("--------------------------\n");
-	for (i=1; i<=a; i++){
-		for (j=1; j<=b; j++){
-			printf("%i ", tab[i][j]);
-		}
-		printf("\n");
-	}
-	*/
 	
 	/*Porownanie kroku poprzedniego z krokiem nastepnym,
 	jesli sa identyczne, to parametr diff2 nie ulegnie zmianie.*/
@@ -100,6 +82,7 @@ int state(int **tab, int a, int b){
 			}
 		}
 	}
+	
 	//Jesli diff1 i diff2 sa rowne 0, oznacza to, ze krok poprzedni i krok nastepny sa takie same.
 	if (diff1 == diff2){
 		stop = 1;
@@ -113,11 +96,8 @@ int state(int **tab, int a, int b){
 
 
 			
-int cont(int m){ //contigent, bo random siê gryzie
-	int seed, r;
-	time_t tt;
-//	seed = time(NULL);
-//	srand(seed);
+int cont(int m){ //Contigent, bo random siê gryzie z niektórymi kompilatorami.
+	int r;
 	r = (rand()%m)+1;
 	return r;
 }
@@ -126,7 +106,7 @@ int cont(int m){ //contigent, bo random siê gryzie
 
 int main(){
 	//zmienne, których czêœæ ma byæ potem wczytywana z pliku
-	int a = 25, b = 25, hm = 200, x, y, c, i, j, count=0, halt=0, alive=0, dead=0, end=0;
+	int a = 25, b = 25, hm = 200, x, y, c, i, j, count=0, halt=0, alive=0, dead=0, end=0, exit=10, step=0;
 	int **place;
 	char com = 'g';
 	
@@ -149,40 +129,22 @@ int main(){
 			i++;
 		}
 	}	
+		
 
-
-//	place[1][2] = 1;
-//	place[2][2] = 1;
-//	place[3][2] = 1;
-	
-	for (i=1; i<=a; i++){
-		for(j=1; j<=b; j++){
-			printf("%i ", place[i][j]);
-		}
-		printf("\n");
-	}
-	
-//	sleep(1);
-//	system("cls");
-	
-	
 	FILE *data = NULL;
 	FILE *output = NULL;
 	data = fopen("config.dat", "r");
 	output = fopen("data.dat", "w+");
 	
-	//inna metoda
+
 	if(data == NULL || output == NULL) {
-	perror("File opening error!\n"); //funkcja wywalajaca komentarz o bledzie
-	return 1; //wyjscie z programu cokolwiek innego w return niz zero, zeby bylo wiadomo w ktorym momencie wyszlo z programu
-	}
-	
+	perror("File opening error!\n");
+	return 1;
+	}	
 
 	board(a,b,place);
 	sleep(1);
-
 	do{
-	//	board(a, b, place);
 		dead = 0;
 		alive = 0;		
 		for (i=1; i<=a; i++){
@@ -194,20 +156,24 @@ int main(){
 				}
 			}
 		}
-	//	printf("%i", count);
-		fprintf(output, "%i %i %i\n", count, dead, alive);
+		fprintf(output, "%i %i %i\n", count, dead, alive); //Zapisywanie do pliku w kolumnach.
 		halt = state(place, a, b);
 		board(a, b, place);
 		count++;
-		com = getchar();
-		if (com == 'e'){
-			break;
+		step++;
+		if (step == exit && exit != 0){
+			step = 0;
+			printf("Do you want to stop the game?\n\
+Type \"e\" and press \"Enter\" to stop, press \"Enter\" to continue.\n");
+			com = getchar();
+			if (com == 'e'){
+				break;
+			}
 		}
 		if (end != 0 && count == end){
 			break;
 		}
 		sleep(1);
-	//	system("pause");
 	} while (halt != 1);
 	
 	printf("Game ended in %i steps.\n", count);	
